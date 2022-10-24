cf197bd950ba9d8b6bffc711a20f52a6e99dd343
ikaslea <ikaslea@ikaslea-SATELLITE-L735>
2022-10-24 17:45:08 +0200

    multiple values of a list test ok

--- src/test/java/TestList.java
+++ src/test/java/TestList.java
@@ -6,18 +6,20 @@
     import org.mockito.Mock;
 import org.mockito.Mockito;  
       
-    public class TestList {  
-      
-        @Test  
-        public void testList_ReturnsSingle_value() {  
-      
-            List mocklist = Mockito.mock(List.class);  
-                               when(mocklist.size()).thenReturn(1);  
-      
-            assertEquals(1, mocklist.size());  
-            assertEquals(1, mocklist.size());  
-              
-                              System.out.println( mocklist.size());  
-            System.out.println(mocklist);  
-        }  
-     }  
+    public class TestList {   
+          
+          @Test   
+          public void testList_Returns_MultipleValues() {  
+            
+          List mocklist = Mockito.mock(List.class);  
+          when(mocklist.size()).thenReturn(1).thenReturn(2).thenReturn(3);  
+            
+          assertEquals(1, mocklist.size());   
+          assertEquals(2, mocklist.size());  
+          assertEquals(3, mocklist.size());  
+            
+          System.out.println(mocklist.size());   
+          System.out.println(mocklist);  
+            
+          }  
+     }    
