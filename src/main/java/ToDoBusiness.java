2aec75ea38fc9357a34b66910ab0b881bb8bad95
ikaslea <ikaslea@ikaslea-SATELLITE-L735>
2022-10-24 17:46:16 +0200

    5. Example of mocking List.get() method

--- src/main/java/ToDoBusiness.java
+++ src/main/java/ToDoBusiness.java
@@ -1,6 +1,6 @@
     import java.util.ArrayList;  
     import java.util.List;  
-      
+      //https://www.javatpoint.com/examples-of-mockito-and-junit-in-eclipse-ide
     // ToDoBusiness is a SUT (system under test)   
     // ToDoService is a Dependency (as ToDoBusiness is dependent on it)  
       
