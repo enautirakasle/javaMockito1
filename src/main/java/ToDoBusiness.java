2f11cbf6cd954d4ccb4cdb8d693b49480d0dab48
ikaslea <ikaslea@ikaslea-SATELLITE-L735>
2022-10-24 18:59:07 +0200

    6. Example of verify() method

--- src/main/java/ToDoBusiness.java
+++ src/main/java/ToDoBusiness.java
@@ -3,6 +3,12 @@
       //https://www.javatpoint.com/examples-of-mockito-and-junit-in-eclipse-ide
     // ToDoBusiness is a SUT (system under test)   
     // ToDoService is a Dependency (as ToDoBusiness is dependent on it)  
+
+    import java.util.ArrayList;  
+    import java.util.List;  
+      
+    // ToDoBusiness is a SUT (system under test)   
+    // ToDoService is a Dependency (as ToDoBusiness is dependent on it)  
       
     public class ToDoBusiness {  
       
@@ -11,18 +17,15 @@
         public ToDoBusiness(ToDoService doService) {  
             this.doService = doService;  
         }  
-          
-        public List<String> getTodosforHibernate(String user) {  
               
-            List<String> hibernatelist = new ArrayList<String>();  
+    public void deleteTodosNotRelatedToHibernate(String user) {  
+              
             List<String> Combinedlist = doService.getTodos(user);  
               
-            for(String todo: Combinedlist) {  
-                if(todo.contains("Hibernate")) {  
-                    hibernatelist.add(todo);  
+            for(String todos:Combinedlist) {  
+                if(!todos.contains("Hibernate")) {  
+                    doService.deleteTodos(todos);  
                 }  
             }  
-              
-            return hibernatelist;  
-            }  
+        }  
      }  
--- src/test/java/ToDoBusinessMock.java
+++ src/test/java/ToDoBusinessMock.java
@@ -1,26 +1,32 @@
-    import static org.junit.Assert.assertEquals;  
+    import static org.mockito.BDDMockito.given;  
     import static org.mockito.Mockito.mock;  
-    import static org.mockito.Mockito.when;  
+    import static org.mockito.Mockito.verify;  
+    import static org.mockito.Mockito.times;
       
     import java.util.Arrays;  
     import java.util.List;  
     import org.junit.Test;  
       
     public class ToDoBusinessMock {  
+          
       
         @Test  
-        public void testusing_Mocks() {  
+        public void deleteTodosusing_BDD() {  
               
-            ToDoService doService = mock(ToDoService.class);  
+            //Given  
+            ToDoService todoService = mock(ToDoService.class);  
                
-            List<String> combinedlist = Arrays.asList(" Use Core Java ", " Use Spring Core ", " Use w3eHibernate ", " Use Spring MVC ");  
-            when(doService.getTodos("dummy")).thenReturn(combinedlist);  
+            List<String> combinedlist = Arrays.asList("Use Hibernate Java", "Use Hibernate Core", "Use Hibernate", "Use Spring MVC");  
               
-            ToDoBusiness business = new ToDoBusiness(doService);  
+                given(todoService.getTodos("dummy")).willReturn(combinedlist);  
+              
+            ToDoBusiness business = new ToDoBusiness(todoService);  
           
-            List<String> alltd = business.getTodosforHibernate("dummy");   
+            //When  
+             business.deleteTodosNotRelatedToHibernate("dummy");  
               
-            System.out.println(alltd);  
-            assertEquals(1, alltd.size());  
-        }  
+            //Then  
+            //verify(todoService).deleteTodos("Use Spring MVC");  
+             verify(todoService, times(1)).deleteTodos("Use Spring MVC");  
+            }  
      }  
