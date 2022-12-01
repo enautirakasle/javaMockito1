fcebea669c5ea2f600f84085ef07831f701946cb
Enaut A A <eagirre@plaiaundi.com>
2022-12-01 10:21:24 +0100

    indentar

--- src/test/java/ToDoBusinessStub.java
+++ src/test/java/ToDoBusinessStub.java
@@ -1,19 +1,19 @@
-    import static org.junit.Assert.*;  
-    import java.util.List;  
-    import org.junit.Test;  
-      
-    public class ToDoBusinessStub {  
-      
-        @Test  
-        public void test() {  
-              
-            ToDoService doServiceStub = new ToDoServiceStub();   
-                  
-            ToDoBusiness business = new ToDoBusiness(doServiceStub);  
-          
-            List<String> retrievedtodos = business.getTodosforSpring(" Dummy ");  
-              
-            assertEquals(2, retrievedtodos.size());  
-           System.out.println(" Stub is working correctly...!!");  
-           }  
-     }  
+import static org.junit.Assert.*;
+import java.util.List;
+import org.junit.Test;
+
+public class ToDoBusinessStub {
+
+	@Test
+	public void test() {
+
+		ToDoService doServiceStub = new ToDoServiceStub();
+
+		ToDoBusiness business = new ToDoBusiness(doServiceStub);
+
+		List<String> retrievedtodos = business.getTodosforSpring(" Dummy ");
+
+		assertEquals(2, retrievedtodos.size());
+		System.out.println(" Stub is working correctly...!!");
+	}
+}
